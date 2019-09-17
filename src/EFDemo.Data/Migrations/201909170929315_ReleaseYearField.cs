9653ba42c155a2765aafc920bfef5cd5815d2ef5
Joao Grassi <joao@tapkey.com>
2019-09-17 11:32:13 +0200

    Upgraded to EF 6.3 6.3.0-rc1; Migrations still work normally via PMC.

---
+++ src/EFDemo.Data/Migrations/201909170929315_ReleaseYearField.cs
@@ -0,0 +1,18 @@
+﻿namespace EFDemo.Data.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class ReleaseYearField : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.Movie", "ReleaseYear", c => c.Int());
+        }
+        
+        public override void Down()
+        {
+            DropColumn("dbo.Movie", "ReleaseYear");
+        }
+    }
+}
